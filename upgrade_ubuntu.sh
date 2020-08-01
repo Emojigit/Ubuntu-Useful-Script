fca17ec44deacab23e392e539ce050c6db7ac462
Emojigit <55009343+Emojigit@users.noreply.github.com>
2020-08-01 22:19:08 +0800

    Create upgrade_ubuntu.sh

---
+++ upgrade_ubuntu.sh
@@ -0,0 +1,11 @@
+# /bin/bash
+if [[ $EUID -ne 0 ]]; then
+   echo "This script must be run as root" 
+   exit 1
+fi
+
+apt-get update
+apt-get upgrade
+apt-get install update-manager-core
+do-release-upgrade
+echo "A reboot is reqired. You can check the upgrade by using this command: `lsb_release -a`"
